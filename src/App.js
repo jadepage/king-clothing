137b12a577a47d824ea68ae44cf62469f6c1658c
Jade Page <jadepage@gmail.com>
2021-08-06 10:57:57 +0200

    updated app with shop page, collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { Switch, Route } from "react-router";
 
 import HomePage from "./pages/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
